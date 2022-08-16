b283b3f79a4c4257f4bffa11e2bacef9691ae596
Kiash254 <codewithkiash@gmail.com>
2022-08-16 22:57:58 +0300

    added some data on templates dyanamic

--- products/views.py
+++ products/views.py
@@ -11,11 +11,41 @@ def product(request):
     return render(request, 'product.html', context)
 
 def cart(request):
-      context={}
-      return render(request, 'cart.html')
+    if request.user.is_authenticated:
+        customer=request.user.customer
+        order,created=Order.objects.get_or_create(customer=customer,complete=False)
+        items=order.orderitem_set.all()
+    else:
+        items=[]
+        order={
+            'get_cart_total':0,'get_cart_items':0,'shipping':False
+        }
+    
+        context={
+            "items":items,
+            "order":order,
+
+        }
+        return render(request, 'cart.html', context)
+      
 
 def checkout(request):
-     context={}
-     return render(request, 'checkout.html')
+
+    if request.user.is_authenticated:
+        customer=request.user.customer
+        order,created=Order.objects.get_or_create(customer=customer,complete=False)
+        items=order.orderitem_set.all()
+    else:
+        items=[]
+        order={
+            'get_cart_total':0,'get_cart_items':0,'shipping':False
+        }
+    
+        context={
+            "items":items,
+            "order":order,
+
+        }
+    return render(request, 'checkout.html',context)
 
 
